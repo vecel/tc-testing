38f1a47b5b65cf1cd20719a612ac85e2367045e3
Mateusz <mateusz.karandys@jetbrains.com>
2024-06-10 10:58:42 +0200

    smart 1.0

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,31 +2,10 @@ import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
-import org.karandys.NameGenerator
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import org.karandys.smart.SmartGenerator
 
 version = "2024.03"
+val generator = SmartGenerator(12345)
 
 project {
 
@@ -36,7 +15,7 @@ project {
 }
 
 object Build : BuildType({
-    name = NameGenerator.createName();
+    name = generator.name
 
     vcs {
         root(HttpsGithubComVecelTcTestingGitRefsHeadsMain)
